8f05b9485b1b8528351621b411ace1ddaf2968d7
Jimmy Xu <xjimmyshcn@gmail.com>
2018-08-11 23:13:40 +0800

    [master] add user_data

--- aws/demo-run-container-in-vm/start-container-with-spot-fleet/launch_template.tf
+++ aws/demo-run-container-in-vm/start-container-with-spot-fleet/launch_template.tf
@@ -17,4 +17,47 @@ resource "aws_launch_template" "LAUNCH_TEMPLATE" {
         Name = "${var.PROJECT_NAME}-SpotFleet-ContainerVM"
       }
     }
+    #the userdata script will be executed as root; if the uesrdata changed, the old instance will be replaced by a new one when apply
+    user_data = <<HEREDOC
+#!/bin/bash
+
+## output userdata log
+exec &> /var/log/userdata.log
+
+echo "START: "`date "+%Y-%m-%d %H:%M:%S"`
+
+## Enable the remote API with TLS authentication
+mkdir -p /etc/docker
+
+cat> /etc/docker/ca.pem <<EOF
+${data.template_file.CERT_CA.rendered}
+EOF
+
+cat> /etc/docker/server.pem <<EOF
+${data.template_file.CERT_SERVER.rendered}
+EOF
+
+cat> /etc/docker/server-key.pem <<EOF
+${data.template_file.CERT_SERVER_KEY.rendered}
+EOF
+
+mkdir -p /run/systemd/system/docker.service.d
+echo -e "[Service]\nEnvironment=DOCKER_OPTS=\"-H tcp://0.0.0.0:5732 --tlsverify --tlscacert=/etc/docker/ca.pem --tlscert=/etc/docker/server.pem --tlskey=/etc/docker/server-key.pem\"" > /run/systemd/system/docker.service.d/remote.conf
+systemctl daemon-reload
+systemctl restart docker
+
+
+## Disable SSH Host Key Checking for user core
+echo -e "Host *\n    StrictHostKeyChecking no" > /home/core/.ssh/config
+chown core:core /home/core/.ssh/config
+
+echo "MIDDLE: "`date "+%Y-%m-%d %H:%M:%S"`
+
+## Start nginx container
+docker run -d -p 80:80 nginx
+
+echo "END: "`date "+%Y-%m-%d %H:%M:%S"`
+
+echo "OS START: "`date -d "$(awk -F. '{print $1}' /proc/uptime) second ago" +"%Y-%m-%d %H:%M:%S"`
+HEREDOC
 }
